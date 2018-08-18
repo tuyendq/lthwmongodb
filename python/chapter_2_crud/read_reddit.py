e9feb44650310d88d47807e8c90a32bf90bb01e0
Tuyen Dang <tuyend@gmail.com>
2018-08-18 11:35:43 +0700

    x220: read_reddit - change to urllib3 and modify request statement

--- python/chapter_2_crud/read_reddit.py
+++ python/chapter_2_crud/read_reddit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import json
-import urllib2
+# import urllib2
+import urllib3
 import pymongo
 
 # connect to mongo
@@ -15,10 +16,13 @@ stories = db.stories
 stories.drop()
 
 # get the reddit home page
-reddit_page = urllib2.urlopen("http://www.reddit.com/r/technology/.json")
+# reddit_page = urllib2.urlopen("https://www.reddit.com/r/technology/.json")
+http = urllib3.PoolManager()
+reddit_page = http.request("GET", "http://www.reddit.com/r/technology/.json")
 
 # parse the json into python objects
-parsed = json.loads(reddit_page.read())
+# parsed = json.loads(reddit_page.read())
+parsed = json.loads(reddit_page.data.decode('utf-8'))
 
 # iterate through every news item on the page
 for item in parsed['data']['children']:
